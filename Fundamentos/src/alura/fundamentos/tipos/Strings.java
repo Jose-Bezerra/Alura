e21cee8e58be09c91d9c5a57359edbf506502ec6
José Bezerra <josbezfi@gmail.com>
2023-04-05 21:49:17 -0300

    Create Strings.java

---
+++ Fundamentos/src/alura/fundamentos/tipos/Strings.java
@@ -0,0 +1,17 @@
+package alura.fundamentos.tipos;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        //Text Blocks é um recurso do java 15
+        //Evita a concatenação com o operador + 
+        String versos = """
+                'Todos os dias quando eu acordo
+                Não tenho mais o tempo que passou
+                Mas tenho todo tempo do mundo'               
+                """;
+
+
+        System.out.println(versos);
+    }
+}
