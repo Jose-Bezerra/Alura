58bc6096c9edf41f8238d91e72a6192c1b3bd355
José Bezerra <josbezfi@gmail.com>
2023-04-18 20:22:02 -0300

    Atualizando a classe Movie

--- screenMatch/src/Main.java
+++ screenMatch/src/Main.java
@@ -4,8 +4,13 @@ public class Main {
         myMovie.name = "Superman II";
         myMovie.releaseYear = 1982;
 
-        System.out.println(myMovie);
-        System.out.println(myMovie.name);
-        System.out.println(myMovie.releaseYear);
+        myMovie.name = "Superman 2";
+        myMovie.avalia(8.0);
+        myMovie.avalia(8.9);
+        myMovie.avalia(10);
+
+        System.out.println("Média de avaliação do filme: " + myMovie.pegaMedia());
+        System.out.println(myMovie.ratingTotal);
+        System.out.println(myMovie.sumRating);
     }
 }
