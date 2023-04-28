30cef794b2c58f5e1a69032f08c6feebba22c2b9
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-04-27 22:32:04 -0300

    Update Main.java

--- screenMatch/src/Main.java
+++ screenMatch/src/Main.java
@@ -2,6 +2,7 @@ import com.alura.screenmatch.modelos.Movie;
 
 public class Main {
     public static void main(String[] args) {
+    	
         Movie myMovie = new Movie();
         myMovie.setName("Superman II");
         myMovie.setReleaseYear(1982);
@@ -10,7 +11,8 @@ public class Main {
         myMovie.avalia(8);
         myMovie.avalia(9);
         myMovie.avalia(7);
-
-        System.out.println("Média de avaliação do filme: " + myMovie.pegaMedia());
+        
+        System.out.println("Média de avaliação do filme: " + myMovie.pegaMedia());  
+        
     }
 }
